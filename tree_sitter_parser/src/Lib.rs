59f7820ca4a7da717fc220d5d66532d8a27127fb
saresend <saresend@usc.edu>
2022-10-30 14:32:40 -0700

    added examples

--- tree_sitter_parser/src/Lib.rs
+++ tree_sitter_parser/src/Lib.rs
@@ -24,3 +24,33 @@ pub fn test_parse() {
 
     println!("{}",root_node.start_position());
 }
+
+
+fn invoke_tree_sitter() {
+    let mut parser = Parser::new();
+
+    let language = unsafe { tree_sitter_rust() };
+    parser.set_language(language).unwrap();
+    
+
+    let source_code = "fn test() {}";
+    let tree = parser.parse(source_code, None).unwrap();
+    let root_node = tree.root_node();
+
+    let mut walker = tree.walk();
+    loop{
+        println!("{}", walker.node().kind());
+        if walker.goto_first_child() == false {
+            break;
+        }
+    }
+
+    println!("{}",root_node.start_position());
+
+
+
+}
+
+
+
+
