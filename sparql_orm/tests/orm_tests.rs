276aed29884a4f9c83dae69a0f48380e82a8eba4
saresend <saresend@usc.edu>
2023-01-07 14:18:18 -0800

    implement write_to_sparql db and implements it for T: SparqlQuery

--- sparql_orm/tests/orm_tests.rs
+++ sparql_orm/tests/orm_tests.rs
@@ -1,10 +1,16 @@
-fn get_update_headers() -> reqwest::HeaderMap {
-    let mut headers = reqwest::HeaderMap::new();
+use sparql_orm::query_build::{QueryFragment, SparqlQuery, run_sparql_generation};
+use reqwest;
+use reqwest::blocking::Response;
+
+fn get_update_headers() -> reqwest::header::HeaderMap {
+    let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert(reqwest::header::CONTENT_TYPE, "application/sparql-update".parse().unwrap());
+    headers
 }
-fn write_to_sparql_db() -> reqwest::Result<_>{
-    let client = reqwest::Client::new();
-    let res = client.post("http://localhost:7878/update");
-    todo!()
+fn write_to_sparql_db<T: SparqlQuery + QueryFragment>(val: T) -> reqwest::Result<Response>{
+    let client = reqwest::blocking::Client::new();
+    let body_content = run_sparql_generation(val);   
+    client.post("http://localhost:7878/update").headers(get_update_headers()).body(body_content).send()
 }
 
 #[test]
