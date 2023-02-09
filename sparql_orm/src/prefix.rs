a54590dd9703fc58e65b438668bbbc67d8f004ca
saresend <saresend@usc.edu>
2023-02-08 17:08:36 -0800

    adding http::Uri as our baseline uri type, and also impl a Prefix type which hold info about valid prefixes

--- sparql_orm/src/prefix.rs
+++ sparql_orm/src/prefix.rs
@@ -4,6 +4,7 @@
 //! to different statements
 //!
 use crate::query_build::QueryFragment;
+use http::uri;
 
 /// Marker trait for any type that can render as a
 /// prefix in a SPQARQL query
@@ -12,10 +13,33 @@ pub trait SPQLPrefixTrait: QueryFragment {}
 /// A type denoting the empty set of prefixes
 pub struct NullPrefixSet {}
 
+
+pub struct Prefix(uri::Uri);
+
 impl SPQLPrefixTrait for NullPrefixSet {}
 
 impl QueryFragment for NullPrefixSet {
     fn generate_fragment(&self, builder: &mut crate::query_build::QueryBuilder) {
         // no-op
+    }
+}
+
+impl QueryFragment for Prefix {
+   fn generate_fragment(&self, builder: &mut crate::query_build::QueryBuilder) {
+       let uri_str = self.0.to_string();
+       builder.write_element(&std::format!("<{}>", &uri_str));
+   } 
+}
+
+
+#[cfg(test)]
+mod tests { 
+    use super::*; 
+    use crate::query_build::gen_fragment;
+    #[test]
+    fn test_basic_prefix_generation() {
+        let test_prefix = Prefix(uri::Uri::try_from("http://purl.org/dc/elements/1.1").unwrap());        
+        let output = gen_fragment(test_prefix);
+        assert_eq!("<http://purl.org/dc/elements/1.1>", output);
     }
 }
